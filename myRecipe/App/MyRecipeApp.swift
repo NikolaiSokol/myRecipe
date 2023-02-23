94c389c241801adb3568411260d19d79d51b141b
Nikolai Sokol <nikolaisokol@icloud.com>
2023-02-23 09:50:27 -0300

    Fix

---
+++ myRecipe/App/MyRecipeApp.swift
@@ -0,0 +1,34 @@
+//
+//  MyRecipeApp.swift
+//  myRecipe
+//
+//  Created by Nikolai Sokol on 18.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct MyRecipeApp: App {
+    private let tabBarCoordinator: RootCoordinator
+    
+    init() {
+        let dependencies = Dependencies()
+        let modulesFactory = ModulesFactory(dependencies: dependencies)
+        let rootModulesFactory = RootModulesFactory(
+            dependencies: dependencies,
+            modulesFactory: modulesFactory
+        )
+        let coordinatorsFactory = CoordinatorsFactory(
+            rootModulesFactory: rootModulesFactory,
+            modulesFactory: modulesFactory
+        )
+        
+        tabBarCoordinator = coordinatorsFactory.makeTabBarCoordinator()
+    }
+    
+    var body: some Scene {
+        WindowGroup {
+            tabBarCoordinator.start()
+        }
+    }
+}
